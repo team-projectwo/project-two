44c5abfa8ef89e0fd5ea5621bf83a8f4e83376a0
Parker Grant <parkergrant@parkers-mbp.uconnect.utah.edu>
2018-08-11 10:29:12 -0600

    coordinates are working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,23 +1,31 @@
 var db = require("../models");
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
+  app.get("/api/examples", function (req, res) {
+    db.Example.findAll({}).then(function (dbExamples) {
       res.json(dbExamples);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
+  app.post("/api/examples", function (req, res) {
+    db.Example.create(req.body).then(function (dbExample) {
       res.json(dbExample);
     });
   });
 
+  app.get("http://localhost:3000/api/coords", function (req, res) {
+    var lat = req.body.lat;
+    var lng = req.body.long;
+
+    res.json(lat);
+    res.json(lng);
+  });
+
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  app.delete("/api/examples/:id", function (req, res) {
+    db.Example.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
       res.json(dbExample);
     });
   });
